5ed5d9583f214cbf6013bad83c6c088797faaade
LTH4823 <peter4823zx@naver.com>
2022-08-22 20:30:24 +0900

    Initial commit

---
+++ src/main/java/org/taerock/paytest/PaytestApplication.java
@@ -0,0 +1,13 @@
+package org.taerock.paytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaytestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/taerock/paytest/PaytestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.taerock.paytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
